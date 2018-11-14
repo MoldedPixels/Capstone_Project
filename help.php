ed096372851a3998984a75be0a2106c0f1cf2f3d
MoldedPixels <43187186+MoldedPixels@users.noreply.github.com>
2018-11-14 11:03:17 -0500

    Help Page
    Page with helpful information for endusers.

---
+++ help.php
@@ -0,0 +1,48 @@
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+  <title>Help!</title>
+  <meta charset="utf-8" />
+  <meta name="Author" content="Arieh Gennello" />
+  <style>
+    a { text-decoration: none; }
+    a:hover { text-decoration: underline; }
+    table.fancy    { border-width: 1px 3px 3px 1px;
+                     border-collapse: collapse;
+                     border: solid blue; }
+    table.fancy td { border: 1px dotted black;
+                     padding: 2px 5px;
+                     margin: 5px 20px;
+                     text-align: right; }
+  </style>
+</head>
+<body>
+<h1>This is the Help Page</h1>
+<div id="Menu" style = "">
+<span style = "color: #000000; font-family: 'Palatino Linotype'; font-size: 15px;">
+<a href = "SteamSpy_Search.html">Home
+</a>
+    |   
+<a href = "help.php">Help
+</a>
+</span>
+</div>
+</br>
+<p>
+	<h2> I cannot find the game I am looking for! </h2>
+	Please be sure you are spelling the name of the game correctly, or search using part of the name to get the game you want!</br>
+	<h2> There is too much data on the screen!</h2>
+	You are most likely getting too many games from your search. Please refine your search to a specific game and the data will be much more compact!</br>
+	<h2> Why is there very little information in the Twitch Data tables?</h2>
+	The Twitch table in the database gets data added to it on each search rather than all at once. Search for the game(s) a few times to get more data. 
+</p>
+</body>
+<footer style="border-top: 2px solid red; margin-top: 1ex;">
+    Arieh Gennello | Felix Cori IV | Karl Kiocho
+
+<span style="float: right;">
+<a href="http://validator.w3.org/check/referer">HTML5</a> /
+<a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3">
+    CSS3 </a>
+</span>
+</footer>
+</html>
